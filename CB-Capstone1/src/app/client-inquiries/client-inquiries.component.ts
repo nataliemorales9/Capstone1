4a9708c976673e7c288ffec56ac8e82c11c150b4
nataliemorales9 <nataliemorales996@gmail.com>
2024-03-25 03:27:14 +0000

    Message1ToCap

---
+++ CB-Capstone1/src/app/client-inquiries/client-inquiries.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'client-inquiries',
+  standalone: true,
+  imports: [],
+  templateUrl: './client-inquiries.component.html',
+  styleUrl: './client-inquiries.component.css'
+})
+export class ClientInquiriesComponent {
+
+}
